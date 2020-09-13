a6ffa544199eccf2abee2a66a9306992829ac7d9
thuylinh1202 <70646301+thuylinh1202@users.noreply.github.com>
2020-09-13 21:04:14 +0700

    first commit

---
+++ chapter1_ex1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>product</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <main>
+            <h1>PRODUCT DISCOUNT CALCULATOR</h1>
+            
+            <label>Product Description</label>
+            <span>Green Tea</span><br>
+            <label>List Price</label>
+            <span>$185.00</span><br>
+            <label>Standard Discount</label>
+            <span>10%</span><br>
+            <label>Discount Amount</label>
+            <span>$18.50</span><br>
+            <label>Discount Price</label>
+            <span>$166.50</span>
+        </main>
+    </body>
+</html>
